ffc4da8fb29a71cfeb47545f66f77ffd2b0c1866
datle <datle@trudi.ai>
2023-12-20 22:04:13 +0700

    fix statistical

--- src/statistical/statistical.service.ts
+++ src/statistical/statistical.service.ts
@@ -12,43 +12,78 @@ export class StatisticalService {
     private readonly orderRepository: Repository<OrderEntity>,
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>
-  ){}
+  ) { }
 
   async statisticalOrder(query) {
     const startDate = new Date(query?.startDate);
     const endDate = new Date(query?.endDate);
-    let totalProfit = 0;
-      const order = await this.orderRepository.createQueryBuilder('order')
-      .where('order.status = :status',{status:Status.DONE})
-      if (startDate && endDate) {
-        if (!isNaN(startDate.getTime()) && !isNaN(endDate.getTime())) {
-          order.andWhere('order.createdTimestamp >= :startDate', { startDate: startDate.getTime() })
-            .andWhere('order.createdTimestamp <= :endDate', { endDate: endDate.getTime() });
+      let totalProfit = 0;
+    if (!startDate && !endDate) {
+      const orders = await this.orderRepository.createQueryBuilder('order')
+        .where('order.status = :status', { status: Status.DONE });
+      const result = await orders.getMany();
+      const monthlyProfit = Array.from({ length: 12 }, (_, month) => ({ month: month + 1, totalProfit: 0 }));
+      result.forEach(item => {
+        const timestampString: any = item.createdTimestamp;
+        const timestamp = parseInt(timestampString, 10);
+        if (!isNaN(timestamp) && timestamp !== null && timestamp !== undefined) {
+          const date = new Date(timestamp);
+          const month = date.getMonth();
+          if (monthlyProfit[month]) {
+            monthlyProfit[month].totalProfit += item.totalPrice;
+          } else {
+            console.error(`monthlyProfit for month ${month} is undefined.`);
+          }
         } else {
-          console.error('Invalid startDate or endDate format');
+          console.error(`Invalid timestamp string for item: ${JSON.stringify(item)}`);
         }
-      }
-      const result = await order.getMany()
-    for (const item of result) {
-      totalProfit += item.totalPrice;
-  }
-    return {totalProfit, count: result.length}
+      });
+
+      return monthlyProfit;
+    } else {
+      const orders = await this.orderRepository.createQueryBuilder('order')
+        .where('order.status = :status', { status: Status.DONE })
+        .andWhere('order.createdTimestamp >= :startDate', { startDate: startDate.getTime() })
+      .andWhere('order.createdTimestamp <= :endDate', { endDate: endDate.getTime() });
+      const result = await orders.getMany()
+      for (const item of result) {
+            totalProfit += item.totalPrice;
+        }
+      return {totalProfit}
+    }
   }
 
-  async statisticalUsers(query){
+  async statisticalUsers(query) {
     const startDate = new Date(query?.startDate);
     const endDate = new Date(query?.endDate);
-      const user = await this.userRepository.createQueryBuilder('user')
-      if (startDate && endDate) {
-        if (!isNaN(startDate.getTime()) && !isNaN(endDate.getTime())) {
-          user.andWhere('user.createdTimestamp >= :startDate', { startDate: startDate.getTime() })
-            .andWhere('user.createdTimestamp <= :endDate', { endDate: endDate.getTime() });
-        } else {
-          console.error('Invalid startDate or endDate format');
-        }
-      }
+    const user = await this.userRepository.createQueryBuilder('user')
+    if (!isNaN(startDate.getTime()) && !isNaN(endDate.getTime())) {
+        user.andWhere('user.createdTimestamp >= :startDate', { startDate: startDate.getTime() })
+          .andWhere('user.createdTimestamp <= :endDate', { endDate: endDate.getTime() });
       const result = await user.getMany()
-    return {count: result.length}
-  }
+      return { count: result.length }
+    }else {
+      const users = await user.getMany()
+      const monthlyCount = Array.from({ length: 12 }, (_, month) => ({ month: month + 1, count: 0 }));
+  // Tính toán số lượng user được tạo trong mỗi tháng
+  users.forEach(user => {
+    const timestampString: any = user.createdTimestamp;
+    const timestamp = parseInt(timestampString, 10);
+    // Kiểm tra xem monthlyCount[userMonth] đã được định nghĩa chưa
+    if (!isNaN(timestamp) && timestamp !== null && timestamp !== undefined) {
+      const date = new Date(timestamp);
+      const month = date.getMonth();
+      if (monthlyCount[month]) {
+        monthlyCount[month].count++;
+      } else {
+        console.error(`monthlyProfit for month ${month} is undefined.`);
+      }
+    } else {
+      console.error(`Invalid timestamp string for item: ${JSON.stringify(user)}`);
+    }
+  });
 
+  return monthlyCount;
+    }
+  }
 }
